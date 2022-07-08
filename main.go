0e199e97049507035aa934fc4c4b175d09c06c98
Tsov9398 <92568491+Tsov9398@users.noreply.github.com>
2022-07-08 22:46:56 +0800

    superUserchange

--- main.go
+++ main.go
@@ -196,11 +196,11 @@ func init() {
 		if err != nil {
 			continue
 		}
-		sus = append(sus, 893683416)
+		sus = append(sus, i)
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
+	 sus = append(sus, 893683416)
 	// sus = append(sus, 87654321)
 
 	if *runcfg != "" {
